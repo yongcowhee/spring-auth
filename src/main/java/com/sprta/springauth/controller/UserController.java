c052bb902234034f45ab7e818a2f39e43a760d80
yongcowhee <yongcowhee@gmail.com>
2023-11-16 01:18:35 +0900

    회원가입 구현

---
+++ src/main/java/com/sprta/springauth/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.sprta.springauth.controller;
+
+import com.sprta.springauth.dto.SignupRequestDto;
+import com.sprta.springauth.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@ModelAttribute SignupRequestDto requestDto){
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+}
