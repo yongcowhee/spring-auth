91966f9cefb749509fda56519716fd85fe7f7b62
yongcowhee <yongcowhee@gmail.com>
2023-11-21 16:57:18 +0900

    modify .gitignore, jwt login

--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -1,11 +1,11 @@
-package com.sprta.springauth.service;
+package com.sparta.springauth.service;
 
-import com.sprta.springauth.dto.LoginRequestDto;
-import com.sprta.springauth.dto.SignupRequestDto;
-import com.sprta.springauth.entity.User;
-import com.sprta.springauth.entity.UserRoleEnum;
-import com.sprta.springauth.jwt.JwtUtil;
-import com.sprta.springauth.repository.UserRepository;
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.dto.LoginRequestDto;
+import com.sparta.springauth.jwt.JwtUtil;
+import com.sparta.springauth.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -60,22 +60,4 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-
-        // 사용자 확인
-        User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-
-        // 비밀번호 확인
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        // 인증이 됐으니 JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        String token = jwtUtil.createToken(user.getUsername(),user.getRole());
-        jwtUtil.addJwtToCookie(token, res);
-    }
 }
--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -3,7 +3,9 @@ let host = 'http://' + window.location.host;
 $(document).ready(function () {
     const auth = getToken();
     if(auth === '') {
-        window.location.href = host + "/api/user/login-page";
+        $('#login-true').show();
+        $('#login-false').hide();
+        // window.location.href = host + "/api/user/login-page";
     } else {
         $('#login-true').show();
         $('#login-false').hide();
@@ -17,6 +19,8 @@ function logout() {
 }
 
 function getToken() {
+
+    // Cookie: AuthoriZation eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
     let auth = Cookies.get('Authorization');
 
     if(auth === undefined) {
